82fa1d70139a6bcba9406c969c19339a7f8f0ab8
AnaMioki <ana.mioki@sptech.school>
2025-05-04 23:20:44 -0300

    Adicionando o script sem a tabela grupo

---
+++ Database/script2.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE ScoutEase;
+
+USE ScoutEase;
+
+CREATE TABLE usuario
+(idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+NomegrupoEscoteiro VARCHAR(45),
+email VARCHAR(45) NOT NULL UNIQUE,
+senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE escoteiro
+(registroEscoteiro CHAR(9) PRIMARY KEY,
+nome VARCHAR(45),
+dataNasc DATE,
+secaoRamo VARCHAR(30),
+nomeResponsavel VARCHAR(45),
+celular VARCHAR(16),
+vencimentoMensalidade DATE,
+fkUsuario INT,
+CONSTRAINT fkEscoteiroUsuario FOREIGN KEY (fkUsuario)
+REFERENCES usuario(idUsuario)
+);
+
+CREATE TABLE mensalidade
+(idMensalidade INT PRIMARY KEY AUTO_INCREMENT,
+valor DECIMAL(10,2),
+dataPagamento DATE,
+statusMensalidade VARCHAR(8),
+CONSTRAINT chkStatus CHECK( statusMensalidade IN ('pago', 'atrasado')),
+fkEscoteiro CHAR(9),
+CONSTRAINT fkMensalidadeEscoteiro FOREIGN KEY (fkEscoteiro)
+REFERENCES escoteiro(registroEscoteiro)
+);
+
+
