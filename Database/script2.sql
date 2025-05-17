67947993512a238ba73c64a375ab0f8509d9c78c
AnaMioki <ana.mioki@sptech.school>
2025-05-17 18:28:56 -0300

    perf: adiciona on delete cascade para garantir integridade dos dados e altera alguns campos para not null

--- Database/script2.sql
+++ Database/script2.sql
@@ -1,20 +1,22 @@
 CREATE DATABASE ScoutEase;
 
 USE ScoutEase;
+SHOW TABLES;
 select * from usuario;
+
 
 CREATE TABLE usuario
 (idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45),
-NomegrupoEscoteiro VARCHAR(45),
+nome VARCHAR(45) NOT NULL,
+NomegrupoEscoteiro VARCHAR(45) NOT NULL,
 email VARCHAR(45) NOT NULL UNIQUE,
 senha VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE escoteiro
 (registroEscoteiro CHAR(9) PRIMARY KEY,
-nome VARCHAR(45),
-dataNasc DATE,
+nome VARCHAR(45) NOT NULL,
+dataNasc DATE NOT NULL,
 secaoRamo VARCHAR(30),
 nomeResponsavel VARCHAR(45),
 celular VARCHAR(16),
@@ -22,17 +24,19 @@ vencimentoMensalidade DATE,
 fkUsuario INT,
 CONSTRAINT fkEscoteiroUsuario FOREIGN KEY (fkUsuario)
 REFERENCES usuario(idUsuario)
+ON DELETE CASCADE
 );
 
 CREATE TABLE mensalidade
 (idMensalidade INT PRIMARY KEY AUTO_INCREMENT,
-valor DECIMAL(10,2),
+valor DECIMAL(10,2) NOT NULL,
 dataPagamento DATE,
 statusMensalidade VARCHAR(8),
 CONSTRAINT chkStatus CHECK( statusMensalidade IN ('pago', 'atrasado')),
 fkEscoteiro CHAR(9),
 CONSTRAINT fkMensalidadeEscoteiro FOREIGN KEY (fkEscoteiro)
 REFERENCES escoteiro(registroEscoteiro)
+ON DELETE CASCADE
 );
 
 
