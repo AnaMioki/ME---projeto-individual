c14ccd1279d35953739ecdd0213dbd1c71335850
AnaMioki <ana.mioki@sptech.school>
2025-05-30 14:11:08 -0300

    remove: tirando arquivos legados do aquatech

--- Plataforma/public/js/mensalidade.js
+++ Plataforma/public/js/mensalidade.js
@@ -4,16 +4,16 @@ function exibirEscoteiros(escoteiros) {
 
     if (escoteiros.length === 0) {
         listaDiv.style.display = 'none';
-        mensagemDiv.style.display = 'block'; // Exibe a mensagem
+        mensagemDiv.style.display = 'block'; 
         mensagemDiv.innerText = 'Adicione um escoteiro';
     } else {
         mensagemDiv.style.display = 'none';
-        listaDiv.style.display = 'block'; // Exibe a lista de escoteiros
+        listaDiv.style.display = 'block'; 
         listaDiv.innerHTML = escoteiros.map(escoteiro => `<p>${escoteiro.nome} - ${escoteiro.registro}</p>`).join('');
     }
 }
 
-// Chama a função de busca assim que a página carregar
+
 window.onload = function() {
     buscarEscoteiros();
 };
